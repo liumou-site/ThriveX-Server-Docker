b8be623466be8c3351f6a06fc06058b50cd03781
坐公交也用券 <liumou.site@qq.com>
2025-02-07 11:21:31 +0800

    增加系统环境变量写入

--- install.py
+++ install.py
@@ -33,20 +33,55 @@ class Install:
         self.dst = "./docker-compose.yml"
     def get_user_input(self):
         if not self.host:
-            self.host = get_user_input("请输入数据库地址: ")
+            if os.getenv("THRIVEX_DB_HOST"):
+                self.host = os.getenv("THRIVEX_DB_HOST")
+            else:
+                self.host = get_user_input("请输入数据库地址: ")
         if not self.port:
-            self.port = get_user_input("请输入数据库端口，默认: 3306: ", default=3306, validation=lambda x: re.match(r"^[0-9]+$", x))
+            if os.getenv("THRIVEX_DB_PORT"):
+                self.port = os.getenv("THRIVEX_DB_PORT")
+            else:
+                self.port = get_user_input("请输入数据库端口，默认: 3306: ", default=3306, validation=lambda x: re.match(r"^[0-9]+$", x))
         if not self.username:
-            self.username = get_user_input("请输入数据库用户名，默认: thrive: ", default="thrive")
+            if os.getenv("THRIVEX_DB_USER"):
+                self.username = os.getenv("THRIVEX_DB_USER")
+            else:
+                self.username = get_user_input("请输入数据库用户名，默认: thrive: ", default="thrive")
         if not self.password:
             self.password = get_user_input("请输入数据库密码: ", validation=lambda x: "|" not in x)
         if not self.dbname:
-            self.dbname = get_user_input("请输入数据库名称，默认: ThriveX: ", default="ThriveX")
+            if os.getenv("THRIVEX_DB_NAME"):
+                self.dbname = os.getenv("THRIVEX_DB_NAME")
+            else:
+                self.dbname = get_user_input("请输入数据库名称，默认: ThriveX: ", default="ThriveX")
         if not self.email:
-            self.email = get_user_input("请输入你的邮箱地址，默认: 123456789@qq.com: ", default="123456789@qq.com", validation=lambda x: "|" not in x)
+            if os.getenv("THRIVEX_EMAIL"):
+                self.email = os.getenv("THRIVEX_EMAIL")
+            else:
+                self.email = get_user_input("请输入你的邮箱地址，默认: 123456789@qq.com: ", default="123456789@qq.com", validation=lambda x: "|" not in x)
         if not self.email_password:
-            self.email_password = get_user_input("请输入你的邮箱密码，默认: 123456789: ", default="123456", validation=lambda x: "|" not in x)
-
+            if os.getenv("THRIVEX_EMAIL_PASSWORD"):
+                self.email_password = os.getenv("THRIVEX_EMAIL_PASSWORD")
+            else:
+                self.email_password = get_user_input("请输入你的邮箱密码，默认: 123456789: ", default="123456", validation=lambda x: "|" not in x)
+    def show(self):
+        print("当前配置:")
+        print(f"数据库地址: {self.host}")
+        print(f"数据库端口: {self.port}")
+        print(f"数据库用户名: {self.username}")
+        print(f"数据库密码: {self.password}")
+        print(f"数据库名称: {self.dbname}")
+        print(f"邮箱地址: {self.email}")
+        print(f"邮箱密码: {self.email_password}")
+        input("按回车键继续,按Ctrl+C取消...")
+    def set_env(self):
+        os.environ["THRIVEX_DB_HOST"] = self.host
+        os.environ["THRIVEX_DB_PORT"] = str(self.port)
+        os.environ["THRIVEX_DB_USER"] = self.username
+        os.environ["THRIVEX_DB_PASSWORD"] = self.password
+        os.environ["THRIVEX_DB_NAME"] = self.dbname
+        os.environ["THRIVEX_EMAIL"] = self.email
+        os.environ["THRIVEX_EMAIL_PASSWORD"] = self.email_password
     def replace_in_file(self, pattern, replacement):
         try:
             with open(self.dst, 'r', encoding='utf-8') as file:
@@ -99,6 +134,8 @@ class Install:
         self.get_docker_status()
         self.copy()
         self.get_user_input()
+        self.show()
+        self.set_env()
         self.replace()
         self.build()
 
