f711cb4768b4ea0e2d4f419b1897d4de5cf5c03e
坐公交也用券 <liumou.site@qq.com>
2025-02-07 11:16:51 +0800

    使用Python实现

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 import os
+from argparse import ArgumentParser
 from shutil import copy2
 import logging
 import sys
@@ -18,62 +19,99 @@ def get_user_input(prompt, default=None, validation=None):
             continue
         return user_input
 
-def replace_in_file(file_path, pattern, replacement):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
+class Install:
+    def __init__(self):
+        self.host = args.host
+        self.port = args.port
+        self.username = args.username
+        self.password = args.password
+        self.dbname = args.dbname
+        self.email = args.email
+        self.email_password = args.email_password
+        # 设置源文件
+        self.src = "docker/docker-compose.yaml"
+        self.dst = "./docker-compose.yml"
+    def get_user_input(self):
+        if not self.host:
+            self.host = get_user_input("请输入数据库地址: ")
+        if not self.port:
+            self.port = get_user_input("请输入数据库端口，默认: 3306: ", default=3306, validation=lambda x: re.match(r"^[0-9]+$", x))
+        if not self.username:
+            self.username = get_user_input("请输入数据库用户名，默认: thrive: ", default="thrive")
+        if not self.password:
+            self.password = get_user_input("请输入数据库密码: ", validation=lambda x: "|" not in x)
+        if not self.dbname:
+            self.dbname = get_user_input("请输入数据库名称，默认: ThriveX: ", default="ThriveX")
+        if not self.email:
+            self.email = get_user_input("请输入你的邮箱地址，默认: 123456789@qq.com: ", default="123456789@qq.com", validation=lambda x: "|" not in x)
+        if not self.email_password:
+            self.email_password = get_user_input("请输入你的邮箱密码，默认: 123456789: ", default="123456", validation=lambda x: "|" not in x)
 
-    content = re.sub(pattern, replacement, content)
+    def replace_in_file(self, pattern, replacement):
+        try:
+            with open(self.dst, 'r', encoding='utf-8') as file:
+                content = file.read()
+                file.close()
+        except Exception as e:
+            logging.error(f"读取文件失败: {e}")
+            sys.exit(1)
+        content = re.sub(pattern, replacement, content)
+        try:
+            with open(self.dst, 'w', encoding='utf-8') as file:
+                file.write(content)
+                file.close()
+        except Exception as e:
+            logging.error(f"写入文件失败: {e}")
+            sys.exit(1)
+    def get_docker_status(self):
+        if subprocess.getstatusoutput("docker ps > /dev/null 2>&1")[0] != 0:
+            logging.error("Docker 未运行，请启动 Docker 后重试。")
+            sys.exit(1)
+    def copy(self):
+        if os.path.isfile(self.src):
+            try:
+                copy2(self.src, self.dst)
+            except Exception as e:
+                logging.error(f"复制文件失败: {e}")
+                sys.exit(1)
+        else:
+            logging.error(f"未找到 {self.src} 文件")
+            sys.exit(1)
+    def replace(self):
+        self.replace_in_file(r"DbHost", self.host)
+        self.replace_in_file(r"Port3306", str(self.port))
+        self.replace_in_file(r"DbNameThriveX", self.dbname)
+        self.replace_in_file(r"DbUserThrive", self.username)
+        self.replace_in_file(r"DB_PASSWORD_ThriveX@123\?", self.password)
+        self.replace_in_file(r"123456789@qq\.com", self.email)
+        self.replace_in_file(r"123456789Password", self.email_password)
+    def build(self):
+        try:
+            result = subprocess.getstatusoutput("docker compose -p thrive up -d --build")
+            if result[0] != 0:
+                logging.error(f"构建失败: {result[1]}")
+                sys.exit(1)
+            logging.info("构建成功")
+        except Exception as e:
+            logging.error(f"构建失败: {e}")
+            sys.exit(1)
+    def start(self):
+        self.get_docker_status()
+        self.copy()
+        self.get_user_input()
+        self.replace()
+        self.build()
 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(content)
-
-def main():
-    src = "docker/docker-compose.yaml"
-    if not os.path.isfile(src):
-        logging.error("未找到 docker-compose.yml 文件")
-        sys.exit(1)
-
-    try:
-        copy2(src, "./docker-compose.yml")
-    except Exception as e:
-        logging.error(f"复制 docker-compose.yml 失败: {e}")
-        sys.exit(1)
-
-    logging.info("正在构建...")
-    if subprocess.getstatusoutput("docker ps > /dev/null 2>&1")[0] != 0:
-        logging.error("Docker 未运行，请启动 Docker 后重试。")
-        sys.exit(1)
-
-    host = get_user_input("请输入数据库地址: ")
-    port = get_user_input("请输入数据库端口，默认: 3306: ", default=3306, validation=lambda x: re.match(r"^[0-9]+$", x))
-    # 端口类型转换
-    try:
-        port = int(port)
-    except ValueError:
-        logging.error("端口格式错误，请输入数字。")
-        sys.exit(1)
-    username = get_user_input("请输入数据库用户名，默认: thrive: ", default="thrive")
-    password = get_user_input("请输入数据库密码: ", validation=lambda x: "|" not in x)
-    dbname = get_user_input("请输入数据库名称，默认: ThriveX: ", default="ThriveX")
-
-    email = get_user_input("请输入你的邮箱地址，默认: 123456789@qq.com: ", default="123456789@qq.com", validation=lambda x: "|" not in x)
-    email_password = get_user_input("请输入你的邮箱密码，默认: 123456789: ", default="123456", validation=lambda x: "|" not in x)
-
-    logging.info("开始替换...")
-    replace_in_file("docker-compose.yml", r"DbHost", host)
-    replace_in_file("docker-compose.yml", r"Port3306", str(port))
-    replace_in_file("docker-compose.yml", r"DbNameThriveX", dbname)
-    replace_in_file("docker-compose.yml", r"DbUserThrive", username)
-    replace_in_file("docker-compose.yml", r"DB_PASSWORD_ThriveX@123\?", password)
-    replace_in_file("docker-compose.yml", r"123456789@qq\.com", email)
-    replace_in_file("docker-compose.yml", r"123456789Password", email_password)
-
-    result = subprocess.getstatusoutput("docker compose -p thrive up -d --build")
-    if result[0] != 0:
-        logging.error(f"构建失败: {result[1]}")
-        sys.exit(1)
-
-    logging.info("构建成功")
 
 if __name__ == "__main__":
-    main()
+    arg = ArgumentParser(description='当前脚本版本: 1.0', prog="ThriveXServer")
+    arg.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+    arg.add_argument('-H', '--host', default=None, help='数据库地址', dest='host')
+    arg.add_argument('-p', '--password', default=None, help='数据库密码', dest='password')
+    arg.add_argument('-u', '--username', default=None, help='数据库用户名', dest='username')
+    arg.add_argument('-d', '--dbname', default=None, help='数据库名称', dest='dbname')
+    arg.add_argument('-e', '--email', default=None, help='邮箱地址', dest='email')
+    arg.add_argument('-E', '--email_password', default=None, help='邮箱密码', dest='email_password')
+    args = arg.parse_args()
+    s = Install()
+    s.start()
